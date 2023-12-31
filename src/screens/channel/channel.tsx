fc3e452573e75e3044b0bbc011bcf011111651f1
ducminhsw <119603933+ducminhsw@users.noreply.github.com>
2023-12-31 22:00:55 +0700

    delete useless files

--- src/screens/channel/channel.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-const Channels = () => {
-  return <>
-    <Outlet />
-  </>;
-};
-
-export default Channels;
--- src/screens/friend/friend-constant.ts
+++
@@ -1,3 +0,0 @@
-export interface IFriendItem {
-    username: string;
-}
